5ad8371d9b3526cdbffd142667fd80008277a8bd
Danynialll <thechudragon@gmail.com>
2025-07-27 21:34:39 +0800

    fixed routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,9 +1,14 @@
 <?php
-
+use Config\Services;
+use CodeIgniter\Config\BaseConfig;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
+$routes = Services::routes();
+if(file_exists(SYSTEMPATH . 'Config/Routes.php')) {
+    require SYSTEMPATH . 'Config/Routes.php';
+}
+
 $routes->get('/', 'Home::index');
-service('auth')->routes();
